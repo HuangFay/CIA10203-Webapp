36a03c7bdc8e138cf539a99ad59c5e996c367127
HuangChen <ss589632@gmail.com>
2024-06-06 20:13:54 +0800

    service

---
+++ src/main/java/com/table_type/model/TableTypeService.java
@@ -0,0 +1,53 @@
+package com.table_type.model;
+
+import java.util.List;
+import java.util.Map;
+
+public class TableTypeService {
+	private TableTypeDAO_interface dao;
+	
+	public TableTypeService() {
+		dao = new TableTypeDAO();
+	}
+	public TableTypeVO addTableType( Integer tableType,Integer tableTypeNumber) {
+		
+		TableTypeVO tableTypeVO = new TableTypeVO();
+		
+		tableTypeVO.setTableType(tableType);
+		tableTypeVO.setTableId(tableTypeNumber);
+		
+		return tableTypeVO;
+	}
+	
+	public TableTypeVO updateTableType(Integer tableId,Integer tableType,Integer tableTypeNumber) {
+		TableTypeVO tableTypeVO = new TableTypeVO();
+		tableTypeVO.setTableId(tableId);
+		tableTypeVO.setTableType(tableType);
+		tableTypeVO.setTableTypeNumber(tableTypeNumber);
+		
+		
+		dao.update(tableTypeVO);
+		return dao.findByPrimaryKey(tableId);
+		
+	}
+	
+	public void deleteTableType(Integer tableId) {
+		dao.delete(tableId);
+	}
+	public TableTypeVO getOneTableType(Integer tableId) {
+		return dao.findByPrimaryKey(tableId);
+	}
+
+	public List<TableTypeVO> getAll() {
+		return dao.getAll();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
